0bf9feb5cc41745f16306b05f3cf394dba97d650
ZitroneMelone <57600331+ZitroneMelone@users.noreply.github.com>
2023-10-14 08:41:14 +0200

    Add files via upload

---
+++ ChatApp/ChatApp/Elements/ChatMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Elements
+{
+    internal class ChatMessage
+    {
+
+
+        private String User { get; set; }
+        private String Message {  get; set; }
+
+        public ChatMessage(string user, string message)
+        {
+            User = user;
+            Message = message;
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ ChatApp/ChatApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ChatApp.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ChatApp
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        private void OnSendMessageClick(object sender, RoutedEventArgs e)
+        {
+            // Get the message from the TextBox
+            string message = MessageTextBox.Text;
+
+            // Check if the message is not empty
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                // Create a new ChatMessage object
+                ChatMessage chatMessage = new ChatMessage(
+                     "User", // Whatever the logged in Users Name is
+                     message
+                );
+
+                // Add the message to the chat ListView (Doesn´t work yet, need to look into Bindings and DataContext first)
+                ChatListView.Items.Add(chatMessage);
+
+                // Clear the message input TextBox
+                MessageTextBox.Clear();
+            }
+
+        }
+        }
+}
